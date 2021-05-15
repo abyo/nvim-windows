ce78d5b66191c61b4b217b618aee5995600668f9
abyo <alexolland@getcodingknowledge.com>
2021-05-15 19:42:07 +0200

    Add new lua package manager (packer)

---
+++ lua/lt/plugins.lua
@@ -0,0 +1,36 @@
+-- Only required if you have packer in your `opt` pack
+local packer_exists = pcall(vim.cmd, [[packadd packer.nvim]])
+
+if not packer_exists then
+  if vim.fn.input("Download Packer? (y for yes)") ~= "y" then
+    return
+  end
+
+  print("Downloading packer.nvim...")
+  local directory = string.format(
+    '%s/site/pack/packer/opt/',
+    vim.fn.stdpath('data')
+  )
+
+  vim.fn.mkdir(directory, 'p')
+
+  local out = vim.fn.system(string.format(
+    'git clone %s %s',
+    'https://github.com/wbthomason/packer.nvim',
+    directory .. '/packer.nvim'
+  ))
+
+  print(out)
+  print("Downloaded packer.nvim")
+  print("Reopen NVIM and run :PackerSync twice")
+
+  return
+end
+
+return require('packer').startup {
+  function(use)
+    use 'rstacruz/vim-closer'
+    use 'tpope/vim-endwise'
+    use 'wakatime/vim-wakatime'
+  end
+}
