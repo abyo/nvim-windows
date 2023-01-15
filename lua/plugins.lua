1caa65024e0ffe2fee4ac5937cde7b48f14ad4f1
gongbaodd <gongbaodd@Inspiron>
2023-01-15 11:37:23 +0800

    remove duplicate plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -31,7 +31,6 @@ return packer.startup(function(use)
   use "wbthomason/packer.nvim"                        -- Packer manage itself
   use "nvim-lua/popup.nvim"                           -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim"                         -- Useful lua functions used by lots of plugins
-  use 'kyazdani42/nvim-web-devicons'                  -- Adds file type icons to Vim plugins
 
   -- General
   use "windwp/nvim-ts-autotag"                        -- Auto clse tags for html
