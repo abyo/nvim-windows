f9cd40be501bfc3b56cd14d9c1b2c2f078df71ee
gongbaodd <gongbaodd@MacMini>
2023-01-18 13:59:19 +0800

    null ls use eslint_d

--- lua/plugins/lsp/null-ls.lua
+++ lua/plugins/lsp/null-ls.lua
@@ -17,8 +17,8 @@ null_ls.setup {
   debug = true,
   sources = {
     null_ls.builtins.formatting.prettier,
-    null_ls.builtins.formatting.eslint,
-    null_ls.builtins.code_actions.eslint,
+    null_ls.builtins.formatting.eslint_d,
+    null_ls.builtins.code_actions.eslint_d,
     null_ls.builtins.formatting.rustfmt,
     null_ls.builtins.diagnostics.eslint_d.with({
       diagnostics_format = '[eslint] #{m}\n(#{c})'
