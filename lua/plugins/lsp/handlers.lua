e44f126473b90a0e6bd57c2b02311ec89a62959c
gongbaodd <gongbaodd@MacMini>
2023-01-18 13:53:55 +0800

    update null-ls

--- lua/plugins/lsp/handlers.lua
+++ lua/plugins/lsp/handlers.lua
@@ -78,7 +78,7 @@ M.on_attach = function(client, bufnr)
     vim.api.nvim_create_autocmd("BufWritePre", {
       group = vim.api.nvim_create_augroup("Format", { clear = true }),
       buffer = bufnr,
-      callback = function() vim.lsp.buf.formatting_seq_sync() end
+      callback = function() vim.lsp.buf.format() end
     })
   end
   if client.name == "rome" then
